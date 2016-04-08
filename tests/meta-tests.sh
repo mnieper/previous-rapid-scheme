459f81ad78ce6c9a9795315530242036330f7385
Marc Nieper-Wisskirchen <marc@nieper-wisskirchen.de>
2016-04-08 17:30:27 +0200

    Add example file.

--- tests/meta-tests.sh
+++ tests/meta-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#bin/rapid-scheme -Ishare -Ischeme-lib -Ischeme-reader -Ischeme-expander -Ilib bin/rapid-compiler.scm > tests/rapid-compiler.bin
+bin/rapid-scheme -Ishare -Ischeme-lib -Ischeme-reader -Ischeme-expander -Ilib bin/rapid-compiler.scm > tests/rapid-compiler.bin
 
-#chibi-scheme -Ischeme-lib -Ilib tests/rapid-compiler.bin -Ishare -Ischeme-lib -Ischeme-reader -Ischeme-expander -Ilib bin/rapid-compiler.scm > tests/rapid-compiler.out
+chibi-scheme -Ischeme-lib -Ilib tests/rapid-compiler.bin -Ishare -Ischeme-lib -Ischeme-reader -Ischeme-expander -Ilib bin/rapid-compiler.scm > tests/rapid-compiler.out
 
 diff tests/rapid-compiler.bin tests/rapid-compiler.out
